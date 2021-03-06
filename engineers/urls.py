ed78a3974ffe2edf20afae1ccd2da5b478e5a791
DimitriiIgnatev <46378328+DimitriiIgnatev@users.noreply.github.com>
2021-03-06 12:08:30 +0100

    urls

--- engineers/urls.py
+++ engineers/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+
 #from django.contrib.auth.decorators import login_required
 import ganttchart.views
 
@@ -35,3 +39,6 @@ urlpatterns = [
         ganttchart.views.create_project_home,
         name='project-create-home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
